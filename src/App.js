eab2ab9a9999d029e20e9b882cc1de4654a8d36a
dk.davidson <13313@coderadademy.edu.au>
2023-05-05 04:45:56 +1000

    added header and content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+let someMessage = "Grey MaTTer!";
 
 function App() {
+  let quizTopics = [
+    "geography",
+    "Anatomy",
+    "History",
+    "Math",
+    "Science",
+    "Lore",
+    "Film",
+    "Music",
+    "General knowledge",
+    "Riddles",
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-      
+        <h1>{someMessage}</h1>
+        <h2>Test your knowledge</h2>
       </header>
+      <div>
+        <ul>
+          {quizTopics.map((topics, index) => (
+            <li key={index}>{topics}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
