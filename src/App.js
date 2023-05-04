9b8aa0f50f953bfa11e069b5d69b0ec6f1668e3d
dk.davidson <13313@coderadademy.edu.au>
2023-05-04 18:38:23 +1000

    list objects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-let someMessage = "Hello People!";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> {someMessage}</h1>
+      
       </header>
     </div>
   );
